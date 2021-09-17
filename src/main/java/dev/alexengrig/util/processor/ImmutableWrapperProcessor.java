898125bc85192b400bd7a99020450c3fc0f54c7f
Grig Alex <alexengrigdev@gmail.com>
2021-09-18 00:52:44 +0300

    Create ImmutableWrapperProcessor.Context

--- src/main/java/dev/alexengrig/util/processor/ImmutableWrapperProcessor.java
+++ src/main/java/dev/alexengrig/util/processor/ImmutableWrapperProcessor.java
@@ -25,6 +25,23 @@ public class ImmutableWrapperProcessor extends AbstractProcessor {
     private static final String WRAPPER_TYPE_PREFIX = "Immutable";
     private static final String JAVA_OBJECT_TYPE_NAME = Object.class.getName();
 
+    // Processor
+
+    @Override
+    public Set<String> getSupportedOptions() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(ANNOTATION_TYPE.getName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.RELEASE_8;
+    }
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         if (roundEnv.processingOver()) {
@@ -37,49 +54,27 @@ public class ImmutableWrapperProcessor extends AbstractProcessor {
         return true;
     }
 
+    // Implementation
+
     private void doProcess(TypeElement typeElement) {
-        String source = createSource(typeElement);
-        JavaFileObject file = createFile(typeElement);
+        Context context = new Context(typeElement);
+        String source = createSource(context);
+        JavaFileObject file = createFile(context);
         writeSourceToFile(source, file);
     }
 
-    private void writeSourceToFile(String source, JavaFileObject file) {
-        try (PrintWriter sourcePrinter = new PrintWriter(file.openWriter())) {
-            sourcePrinter.print(source);
-        } catch (IOException e) {
-            throw new RuntimeException("Fail on writing file", e);
-        }
-    }
-
-    private String createFilename(TypeElement typeElement) {
-        String prefixedClassname = getPrefixedClassName(typeElement);
-        Optional<String> packageNameOptional = getPackageName(typeElement);
-        return packageNameOptional
-                .map(packageName -> packageName.concat(".").concat(prefixedClassname))
-                .orElse(prefixedClassname);
-    }
-
-    private JavaFileObject createFile(TypeElement typeElement) {
-        String filename = createFilename(typeElement);
-        try {
-            return processingEnv.getFiler().createSourceFile(filename);
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Fail on creating file", e);
-        }
-    }
-
-    private String createSource(TypeElement typeElement) {
+    private String createSource(Context context) {
         StringJoiner joiner = new StringJoiner("\n");
-        getPackageName(typeElement).ifPresent(packageName ->
+        context.getPackageName().ifPresent(packageName ->
                 joiner.add("package " + packageName + ";"));
-        String className = getPrefixedClassName(typeElement);
-        String parentClassName = getClassName(typeElement);
+        String className = context.getTargetSimpleClassName();
+        String parentClassName = context.getDomainClassName();
         joiner.add("public class " + className + " extends " + parentClassName + " {");
         joiner.add("    private final " + parentClassName + " target;");
         joiner.add("    public " + className + "(" + parentClassName + " target) {");
         joiner.add("        this.target = target;");
         joiner.add("    }");
-        for (ExecutableElement method : getAllMethods(typeElement)) {
+        for (ExecutableElement method : context.getAllMethods()) {
             joiner.add("    @java.lang.Override");
             String accessModifier = getAccessModifier(method)
                     .map(a -> a.concat(" "))
@@ -127,85 +122,127 @@ public class ImmutableWrapperProcessor extends AbstractProcessor {
                 .collect(Collectors.joining(", "));
     }
 
-    private Collection<ExecutableElement> getAllMethods(TypeElement typeElement) {
-        Map<String, ExecutableElement> methods = getMethods(typeElement)
-                .collect(Collectors.toMap(this::getKey, Function.identity()));
-        Optional<TypeElement> parent = getParent(typeElement);
-        if (!parent.isPresent()) {
-            return methods.values();
-        }
-        for (TypeElement element = parent.get(); element != null; element = getParent(element).orElse(null)) {
-            getMethods(element)
-                    .filter(executableElement -> !methods.containsKey(getKey(executableElement)))
-                    .forEach(executableElement -> methods.put(getKey(executableElement), executableElement));
-        }
-        return methods.values();
-    }
-
-    private String getKey(ExecutableElement m) {
-        return m.toString();
-    }
-
-    private Optional<String> getPackageName(TypeElement typeElement) {
-        String qualifiedName = typeElement.getQualifiedName().toString();
-        int lastIndexOfDot = qualifiedName.lastIndexOf('.');
-        if (lastIndexOfDot == -1) {
-            return Optional.empty();
-        }
-        return Optional.of(qualifiedName.substring(0, lastIndexOfDot));
-    }
-
-    private String getClassName(TypeElement typeElement) {
-        return typeElement.getQualifiedName().toString();
-    }
-
-    private String getSimpleClassName(TypeElement typeElement) {
-        return typeElement.getSimpleName().toString();
-    }
-
-    private String getPrefixedClassName(TypeElement typeElement) {
-        String simpleClassName = getSimpleClassName(typeElement);
-        return WRAPPER_TYPE_PREFIX.concat(simpleClassName);
-    }
-
-    private Stream<ExecutableElement> getMethods(TypeElement typeElement) {
-        return typeElement.getEnclosedElements().stream()
-                .filter(e -> ElementKind.METHOD.equals(e.getKind()))
-                .map(ExecutableElement.class::cast)
-                .filter(m -> !m.getModifiers().contains(Modifier.STATIC))
-                .filter(m -> !m.getModifiers().contains(Modifier.PRIVATE));
-    }
-
-    private Optional<TypeElement> getParent(TypeElement typeElement) {
-        return processingEnv.getTypeUtils().directSupertypes(typeElement.asType()).stream()
-                .limit(1) // parent class is first
-                .map(DeclaredType.class::cast)
-                .map(DeclaredType::asElement)
-                .filter(element -> element.getKind().isClass())
-                .filter(element -> !JAVA_OBJECT_TYPE_NAME.equals(element.toString()))
-                .map(TypeElement.class::cast)
-                .findFirst();
-    }
-
-    // Processor
-
-    @Override
-    public Set<String> getSupportedOptions() {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public Set<String> getSupportedAnnotationTypes() {
-        return Collections.singleton(ANNOTATION_TYPE.getName());
-    }
-
-    @Override
-    public SourceVersion getSupportedSourceVersion() {
-        return SourceVersion.RELEASE_8;
-    }
-
     @Override
     public Iterable<? extends Completion> getCompletions(Element element, AnnotationMirror annotation, ExecutableElement member, String userText) {
         return Collections.emptySet();
+    }
+
+    private JavaFileObject createFile(Context context) {
+        String filename = context.getTargetClassName();
+        try {
+            return processingEnv.getFiler().createSourceFile(filename);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Fail on creating file: " + filename, e);
+        }
+    }
+
+    private void writeSourceToFile(String source, JavaFileObject file) {
+        try (PrintWriter sourcePrinter = new PrintWriter(file.openWriter())) {
+            sourcePrinter.print(source);
+        } catch (IOException e) {
+            throw new RuntimeException("Fail on writing file", e);
+        }
+    }
+
+    private class Context {
+        private final TypeElement domainTypeElement;
+
+        private transient String domainSimpleClassName;
+        private transient String targetSimpleClassName;
+
+        private transient String domainClassName;
+        private transient String targetClassName;
+
+        @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+        private transient Optional<String> packageNameOptional;
+
+        private Context(TypeElement domainTypeElement) {
+            this.domainTypeElement = domainTypeElement;
+        }
+
+        String getDomainSimpleClassName() {
+            if (domainSimpleClassName == null) {
+                domainSimpleClassName = domainTypeElement.getSimpleName().toString();
+            }
+            return domainSimpleClassName;
+        }
+
+        String getTargetSimpleClassName() {
+            if (targetSimpleClassName == null) {
+                targetSimpleClassName = WRAPPER_TYPE_PREFIX.concat(getDomainSimpleClassName());
+            }
+            return targetSimpleClassName;
+        }
+
+        String getDomainClassName() {
+            if (domainClassName == null) {
+                domainClassName = domainTypeElement.getQualifiedName().toString();
+            }
+            return domainClassName;
+        }
+
+        String getTargetClassName() {
+            if (targetClassName == null) {
+                String className = getTargetSimpleClassName();
+                Optional<String> packageNameOptional = getPackageName();
+                targetClassName = packageNameOptional
+                        .map(packageName -> packageName.concat(".").concat(className))
+                        .orElse(className);
+            }
+            return targetClassName;
+        }
+
+        @SuppressWarnings("OptionalAssignedToNull")
+        Optional<String> getPackageName() {
+            if (packageNameOptional == null) {
+                String qualifiedName = getDomainClassName();
+                int lastIndexOfDot = qualifiedName.lastIndexOf('.');
+                if (lastIndexOfDot == -1) {
+                    packageNameOptional = Optional.empty();
+                } else {
+                    packageNameOptional = Optional.of(qualifiedName.substring(0, lastIndexOfDot));
+                }
+            }
+            return packageNameOptional;
+        }
+
+        Collection<ExecutableElement> getAllMethods() {
+            Map<String, ExecutableElement> methods = getMethods(domainTypeElement)
+                    .collect(Collectors.toMap(this::getKey, Function.identity()));
+            Optional<TypeElement> parent = getParent(domainTypeElement);
+            if (!parent.isPresent()) {
+                return methods.values();
+            }
+            for (TypeElement element = parent.get(); element != null; element = getParent(element).orElse(null)) {
+                getMethods(element)
+                        .filter(executableElement -> !methods.containsKey(getKey(executableElement)))
+                        .forEach(executableElement -> methods.put(getKey(executableElement), executableElement));
+            }
+            return methods.values();
+        }
+
+        private Stream<ExecutableElement> getMethods(TypeElement typeElement) {
+            return typeElement.getEnclosedElements().stream()
+                    .filter(e -> ElementKind.METHOD.equals(e.getKind()))
+                    .map(ExecutableElement.class::cast)
+                    .filter(m -> !m.getModifiers().contains(Modifier.STATIC))
+                    .filter(m -> !m.getModifiers().contains(Modifier.PRIVATE));
+        }
+
+        private Optional<TypeElement> getParent(TypeElement typeElement) {
+            return processingEnv.getTypeUtils().directSupertypes(typeElement.asType()).stream()
+                    .limit(1) // parent class is first
+                    .map(DeclaredType.class::cast)
+                    .map(DeclaredType::asElement)
+                    .filter(element -> element.getKind().isClass())
+                    .filter(element -> !JAVA_OBJECT_TYPE_NAME.equals(element.toString()))
+                    .map(TypeElement.class::cast)
+                    .findFirst();
+        }
+
+        private String getKey(ExecutableElement m) {
+            return m.toString();
+        }
+
     }
 }
